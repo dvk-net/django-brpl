a3885306cd889486447642c7d352761ecdcc50c0
dvk-net <dvk-net@users.noreply.github.com>
2024-12-23 23:59:30 +0000

    deploy: ab8572f358b5a5f61a806bb9e23d969406289231

--- src/settings.py
+++
@@ -1,7 +0,0 @@
-import logging
-from pathlib import Path
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-BASE_DIR = Path(__file__).resolve().parent
-DEFAULT_PROJECT_NAME = "proj"
